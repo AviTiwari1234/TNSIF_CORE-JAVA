413d343697d4d3ae3f15b528d9b39b3e302cdf57
Admin <Admin@LAPTOP-9MJCPV4E>
2024-04-02 19:08:48 +0530

    First commit

---
+++ SFHYD01/src/com/tnsif/dayone/FirstProgram.java
@@ -0,0 +1,17 @@
+package com.tnsif.dayone;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Hyderabad Girls!!!");
+		
+		Scanner s=new Scanner(System.in);
+		
+		String s="Avantika";
+	}
+
+}
